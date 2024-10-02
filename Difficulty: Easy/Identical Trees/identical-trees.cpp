0677ff103ea4912aa383b55f65217431dcf1800d
Kanishka Saket <134317281+Kanisaket10@users.noreply.github.com>
2024-10-02 18:00:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.cpp
+++ Difficulty: Easy/Identical Trees/identical-trees.cpp
@@ -43,11 +43,12 @@ class Solution
     {
         //Your Code here
         if(!r1 && !r2) return 1;
-        if((r1&& !r2) || !r1 && r2) return 0;
+        if((!r1 && r2) || (r1 && !r2)) return 0;
+        
         if(r1->data != r2->data) return 0;
         
-        return (isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right));
         
+        return (isIdentical(r1->left, r2->left) && isIdentical(r1->right, r2->right));
     }
 };
 
